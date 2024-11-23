6efd420225eb458d0ba16f00f5cc170981c6494c
0xprasanth <raviprasanth45@gmail.com>
2024-11-23 21:36:39 +0530

    add in shadcn and revalidate after adding new items from database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,8 +20,15 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
